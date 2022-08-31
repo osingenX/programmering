0525323e93b04655c1bb30685570e076664d8890
Oskar Sjölin <oskar.sjolin@elev.ga.ntig.se>
2022-08-31 14:28:53 +0200

    uwu

---
+++ test/test/Program.ovning.cs
@@ -0,0 +1,22 @@
+﻿string namn = ""; 
+string favoritglass = "";
+string favoritfilm = "";
+string favoritskor = ""; 
+
+Console.WriteLine("Hej, Vad heter du?"); 
+namn = Console.ReadLine();
+
+Console.WriteLine($"Hej {namn}, vad är din favorit glass?:"); 
+favoritglass = Console.ReadLine(); 
+
+Console.WriteLine("Du har dålig smak du, vilken film gillar du mest?:");
+favoritfilm = Console.ReadLine(); 
+
+Console.WriteLine("Ännu sämre än din favoritglass, har du ett par favorit skor?:");
+favoritskor = Console.ReadLine(); 
+
+Console.WriteLine($"Hallå {namn}, hur kan du ens tycka om något av dessa?.");
+Console.WriteLine($"Du har så kass smak att alla skrattar åt dig!");
+
+Console.WriteLine("Tryck på ENTER för att avsluta.");
+Console.ReadLine();
